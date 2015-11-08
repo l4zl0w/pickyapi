886e2ce243165b8405f08c1687b289bd6811c237
Laszlo Bozzay <bozzay.laszlo@gmail.com>
2015-11-08 16:20:52 +0000

    Logging use

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use Socialite;
+use Log;
 
 class AuthController extends Controller
 {
